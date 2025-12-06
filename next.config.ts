6eec7b618bafc4fa90a51ead5df5ad4d9d6c45c3
s2216236997-code <s2216236997@gmail.com>
2025-12-06 21:41:52 +0800

    fix: remove experimental.appDir

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  // 千万不要加 experimental.appDir，也不要写 output: 'export'
 };
 
 export default nextConfig;
