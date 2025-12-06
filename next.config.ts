58c074ee6747736831304a38234836a3d6a8b601
s2216236997-code <s2216236997@gmail.com>
2025-12-06 21:36:37 +0800

    revert: remove experimental.appDir (app router is default)

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
+  // 千万不要加 experimental.appDir，也不要写 output: 'export'
 };
 
 export default nextConfig;
